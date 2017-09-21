5b5f24c69e5da0ce869e089d5c5a16ed60d0ef9f
manu <manu@enigmasolutions.info>
2017-09-21 15:08:55 +0100

    sudo for git pull in script

--- pullAndPublish.sh
+++ pullAndPublish.sh
@@ -4,5 +4,5 @@ echo ----------------------------------------------------
 echo changing directory to /data/cas-overlay-template ...
 cd /data/cas-overlay-template
 echo pulling Changes ...
-git pull chs master
+sudo git pull chs master
 ./publish.sh
